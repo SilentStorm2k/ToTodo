8182ed57b60d775f9b530fc0b5cb08431d673cd6
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-03 21:51:58 +0530

    basic structure of header, body, footer in index.ts and its color palette

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,31 +1,49 @@
 import "../css/styles.css";
-import { createTodo } from "./todo_item";
+import { Footer } from "./footer";
+import { Header } from "./header";
+import todoInputForm from "./todoInputForm";
+import reset from "./reset";
+import createTodo from "./todo_item";
 import { todoList } from "./todo_list";
 
-let todo = createTodo(
-	"1",
-	"title",
-	"description",
-	1,
-	new Date(2025, 4, 3),
-	3,
-	"Proj"
-);
+function test() {
+	let todo = createTodo(
+		"title",
+		"description",
+		1,
+		new Date(2025, 4, 3),
+		3,
+		"Proj"
+	);
 
-todoList.addTodo(todo);
-todoList.addTodo({ ...todo, id: "2", priority: 5 });
-todoList.addTodo({ ...todo, id: "3", priority: 3 });
-todoList.addTodo({ ...todo, id: "4" });
-todoList.addTodo({ ...todo, id: "5" });
+	todoList.addTodo(todo);
+	todoList.addTodo({ ...todo, id: "2", priority: 5 });
+	todoList.addTodo({ ...todo, id: "3", priority: 3 });
+	todoList.addTodo({ ...todo, id: "4" });
+	todoList.addTodo({ ...todo, id: "5" });
 
-console.log(todoList.getTodo("1"));
+	console.log(todoList.getTodo("1"));
 
-console.log("hello world");
+	console.log("hello world");
 
-console.log(todoList.getAllTodos());
+	console.log(todoList.getAllTodos());
 
-let todos = todoList.getAllTodos();
+	let todos = todoList.getAllTodos();
 
-todos.sort((a, b) => a.priority - b.priority);
+	todos.sort((a, b) => a.priority - b.priority);
 
-console.log(todos);
+	console.log(todos);
+}
+
+test();
+
+export function init() {
+	reset();
+	// addFavicon();
+	Header();
+	// body();
+	todoInputForm();
+	Footer();
+}
+
+init();
