7a17a03f899e497a75534065497b526472466be3
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-14 13:55:51 +0530

    updated deploy commit command

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -5,15 +5,15 @@ build_command() {
   pnpm run build
 }
 
-# echo -e "\033[0;32mDeleting existing $branch...\033[0m"
-# git push origin --delete $branch
-# git branch -D $branch
+echo -e "\033[0;32mDeleting existing $branch...\033[0m"
+git push origin --delete $branch
+git branch -D $branch
 
-# echo -e "\033[0;32mSetting up new $branch branch\033[0m"
-# git checkout --orphan $branch
-# git reset --hard
-# git commit --allow-empty -m "Init"
-# git checkout main 
+echo -e "\033[0;32mSetting up new $branch branch\033[0m"
+git checkout --orphan $branch
+git reset --hard
+git commit --allow-empty -m "Init"
+git checkout main 
 
 echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
@@ -25,10 +25,10 @@ echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-# cd $directory && 
-# 	git add --all  &&
-# 	git commit -m "Deploy updates" &&
-# 	git push origin $branch 
+cd $directory && 
+	git add --all  &&
+	git commit -m "Deploy updates" &&
+	git push origin $branch 
 
 echo -e "\033[0;32mCleaning up...\033[0m"
 git worktree remove $directory --force
