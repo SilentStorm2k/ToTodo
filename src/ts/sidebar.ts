0445cf2e703a3232164c0909357cf7191468552c
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-15 20:01:39 +0530

    [feat] logic for projects in project dropdown added

--- src/ts/sidebar.ts
+++ src/ts/sidebar.ts
@@ -1,5 +1,8 @@
 import { todoList } from "./todo_list";
 import "../css/sidebar.styles.css";
+import { cleanElement } from "./reset";
+import { Render } from "./renderTodos";
+import { assert } from "chai";
 
 export const Sidebar = (function () {
 	const addProject = (project: string) => {
@@ -29,15 +32,68 @@ export const Sidebar = (function () {
 	};
 
 	const createInbox = () => {
-		const inbox = document.createElement("div");
+		const inboxButton = document.createElement("button");
+		inboxButton.innerText = "Inbox";
+		inboxButton.addEventListener("click", openInbox);
+		return inboxButton;
+	};
 
-		return inbox;
+	const openInbox = (e: MouseEvent) => {
+		const todoContainer = document.querySelector(
+			".todoContainer"
+		) as HTMLElement;
+		cleanElement(todoContainer);
+		const allTodos = todoList.getAllTodos();
+		Render.showTodos(allTodos);
 	};
 
 	const createProjects = () => {
 		const projects = document.createElement("div");
-
+		const text = document.createElement("div");
+		const projectButtonContainer = document.createElement("div");
+		const projectDropdownButton = document.createElement("button");
+		const projectDropdown = document.createElement("div");
+		projectButtonContainer.appendChild(text);
+		projectButtonContainer.appendChild(projectDropdownButton);
+		projectDropdown.classList.add("projectDropdownClose");
+		projects.appendChild(projectButtonContainer);
+		projects.appendChild(projectDropdown);
+		projectDropdownButton.addEventListener("click", toggleProjectDropdown);
+		populateProjectDropdown(projectDropdown);
 		return projects;
+	};
+
+	const toggleProjectDropdown = (e: MouseEvent) => {
+		const projectDropdownButton = e.target as HTMLElement;
+		const projectButtonContainer = projectDropdownButton.parentElement;
+		const projectDropdownDiv = projectButtonContainer?.children[1];
+		if (projectDropdownDiv?.classList.contains("projectDropdownClose")) {
+			projectDropdownDiv.classList.add("projectDropdownOpen");
+			projectDropdownDiv.classList.remove("projectDropdownClose");
+		} else {
+			assert(
+				projectDropdownDiv?.classList.contains("projectDropdownOpen")
+			);
+			projectDropdownDiv?.classList.remove("projectDropdownOpen");
+			projectDropdownDiv?.classList.add("projectDropdownClose");
+		}
+	};
+
+	const populateProjectDropdown = (projectDropdown: HTMLElement) => {
+		const projects = todoList.getProjectNames();
+		const unorderedList = document.createElement("ul");
+		for (const projectId in projects) {
+			const listElement = document.createElement("li");
+			const projectButton = document.createElement("button");
+			console.log(todoList.getProject(projects[projectId]));
+			projectButton.addEventListener("click", () => {
+				Render.showTodos(todoList.getProject(projects[projectId]));
+			});
+			projectButton.innerText = projects[projectId];
+			listElement.appendChild(projectButton);
+			unorderedList.appendChild(listElement);
+		}
+		projectDropdown.appendChild(unorderedList);
 	};
 
 	const init = () => {
